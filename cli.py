f537e796bdff53097a4c8593eff4da865c304b30
Noah Stieler <noah.stieler@gmail.com>
2023-08-18 11:58:15 -0500

    Support for relative paths. bug fixes.

--- cli.py
+++ cli.py
@@ -10,9 +10,8 @@ import database
 import sys
 import os
 
+# URI for local connection
 # uri = 'mongodb://localhost:27017'
-
-# TODO allow for relative paths
 
 args = {
     'uri': '',
@@ -41,13 +40,13 @@ def _parse_input() -> None:
 
     if len(sys.argv) <= 1:
         print('Invalid command.')
-        exit()
+        sys.exit()
 
     if sys.argv[1] == 'insert':
         _cmd_insert()
     else:
         print('Invalid command.')
-        exit()
+        sys.exit()
 
 
 def _cmd_insert() -> None:
@@ -64,10 +63,21 @@ def _cmd_insert() -> None:
                     valid_param_name = True
 
             if valid_param_name:
-                args[sys.argv[i][2:]] = sys.argv[i + 1]
+                try:
+                    args[sys.argv[i][2:]] = sys.argv[i + 1]
+                except IndexError as e:
+                    print('Invalid number of parameters.')
+                    sys.exit()
             else:
                 print(f'Unrecognized parameter name: \'{sys.argv[i]}\'')
-                exit()
+                sys.exit()
+
+    _check_for_missing_args()
+
+    # Convert all relative paths to absolute paths
+    args['inc_path'] = os.path.abspath(args['inc_path'])
+    args['cal_path'] = os.path.abspath(args['cal_path'])
+    args['data_path'] = os.path.abspath(args['data_path'])
 
     if not os.path.isdir(args['inc_path']):
         print('Directory not found: \'' + args['inc_path'] + '\'')
@@ -79,8 +89,6 @@ def _cmd_insert() -> None:
         print('Directory not found: \'' + args['data_path'] + '\'')
         args['data_path'] = ''
 
-    _check_for_missing_args()
-
 
 def _check_for_missing_args():
     quit_program = False
@@ -90,4 +98,5 @@ def _check_for_missing_args():
             quit_program = True
 
     if quit_program:
-        exit()
+        print('Type adbm help for more information.')
+        sys.exit()
